25be4d847ee23240e8600a2e59f39d6849e404dd
DaraghColl <daraghcoll@hotmail.com>
2020-07-26 00:02:58 +0100

    added path to tag template for link to post

--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -34,7 +34,8 @@ export default {
             ... on Post {
               title
               description
-              date
+              date,
+              path,
             }
           }
         }
